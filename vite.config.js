1a9b6f8de9b971b1abe6c93731da286df4640d31
shahid378 <shahid.mansoori.378@gmail.com>
2024-03-27 19:21:16 +0530

    added deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  base: "/portfolio/",
   server: {
     open: true,
     host: true
