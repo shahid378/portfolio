8878ca6b565796ea43bd68cab8cc396173c56154
shahid378 <shahid.mansoori.378@gmail.com>
2024-03-27 19:28:22 +0530

    removed base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   
-  base: "/portfolio/",
+  // base: "/portfolio/",
   server: {
     open: true,
     host: true
